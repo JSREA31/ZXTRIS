84c8fc48d283d22d0d04a437c98f9a4f8f550f2b
JSREA31 <65662911+JSREA31@users.noreply.github.com>
2022-09-17 13:32:57 +0100

    update

--- hello.asm
+++ hello.asm
@@ -24,33 +24,61 @@ code_start
 	include vars.asm
 
 main_loop	
-	
 	;title screen - goes here
 wait_spacebar
 	call bump_tetro_counter ; sudo randon number for next tetro
 	call get_keyboard
 		cp a, $00
 		jp nz,wait_spacebar
-
 	call init_game
 
 
 
 game_loop	
-	call bump_tetro_counter ; sudo randon number for next tetro
 ;update I'm alive counter
 	ld a,(tetro_counter)
 	ld (Display+1),a
 ;end of I'm alive counter
 
-	call get_keyboard
-	;code to display keypress
-		ld (Display+34),a
-		cp a, $39
-		jp nz, 1F
-			ld a,(tetro_counter)
-			ld(current_tetro),a
+;delay loop	
+	ld a, (delay_counter)
+	dec a
+	ld (delay_counter),a
+	jp nz, 1F
+		ld a,LEVEL1_DELAY
+		ld (delay_counter),a
+		call drop_tetro
 1	
+	call bump_tetro_counter ; sudo randon number for next tetro
+
+
+
+	
+	call get_keyboard
+	
+	;code to display keypress
+	
+	cp a,$FF
+	jp z, 1F	
+	;ld (Display+34),a
+	cp a,$72
+	jp nz, 2F
+		ld a,$13
+		ld (Display+34),a
+		jp 1F
+2	
+	cp a,$73
+	jp nz, 3F
+		ld a,$12
+		ld (Display+34),a
+		jp 1F
+3
+	cp a, $39
+	jp nz, 1F
+		ld a,(tetro_counter)
+		ld(current_tetro),a
+1	
+
 	call render_tetro
 	call render_playfield
 	
@@ -92,7 +120,7 @@ render_tetro
 	ld a,(current_tetro)
 	cp a,0
 	jp z,2F
-	ld de,tetro_block*4
+	ld de,TETRO_BLOCK*4
 1	
 		adc hl,de
 		dec a
@@ -103,7 +131,7 @@ render_tetro
 	ld a,(tetro_rotation)
 	cp a,0
 	jp z,2F
-	ld de,tetro_block
+	ld de,TETRO_BLOCK
 1	
 		adc hl,de
 		dec a
@@ -116,7 +144,7 @@ render_tetro
 	ld a,(tetro_y)
 	cp a,0
 	jp z,2F
-	ld de,well_width+2	
+	ld de,WELL_WIDTH+2	
 1	
 		adc hl,de
 		dec a
@@ -130,30 +158,28 @@ render_tetro
 
 ;now draw the tetro into playfield
 	pop de
-	ld b,tetro_size
+	ld b,TETRO_SIZE
 1
-	ld c,tetro_size	
+	push bc
+	ld b,TETRO_SIZE	
 2		
 		ld a,(de)
 		cp a, $40
-		jp nz,3F
-			ld a,$00
-3		
+		jp z,3F	
 		cp a,$23
-		jp nz,4F
-			ld a,well_wall_char
-4		
-		ld (hl),a
+		jp nz,3F
+			ld a,WELL_WAL_CHAR
+			ld (hl),a
+3		
 		inc hl
 		inc de
-		dec c
-		jp nz, 2B
+		djnz 2B
 	push de
-	ld de, well_width+2-tetro_size
+	ld de, WELL_WIDTH+2-TETRO_SIZE
 	add hl,de
 	pop de
-	dec b
-	jp nz,1B
+	pop bc
+	djnz 1B
 	ret
 
 
@@ -164,14 +190,14 @@ render_playfield
 
 ;get start position in screen memory
 	ld hl,Display
-	ld de,(start_row * screen_width)+start_column+1
+	ld de,(START_ROW * SCREEN_WIDTH)+START_COLUMN+1
 	add hl,de
 
 ;get start of playfield
 	ld de,playfield	
-	ld b, well_height+1
+	ld b, WELL_HEIGHT+1
 next_row_render
-	ld c, well_width+2
+	ld c, WELL_WIDTH+2
 next_column_render
 		ld a,(de)
 		ld (hl),a
@@ -179,12 +205,12 @@ next_column_render
 		inc de
 		dec c
 		jp nz,next_column_render
-	push de
-	ld de, screen_width-(well_width+2)
+		push de
+	ld de, SCREEN_WIDTH-(WELL_WIDTH+2)
 	add hl,de
 	pop de
 	dec b
-	jp nz, next_row_render
+		jp nz, next_row_render
 
 
 	ret
@@ -197,11 +223,11 @@ next_column_render
 new_Well
 ;clear the entire playfied	
 	ld hl,playfield
-	ld b, well_height
+	ld b, WELL_HEIGHT
 next_row_well
-	ld c, well_width+2
+	ld c, WELL_WIDTH+2
 next_column_well
-		ld (hl),well_space
+		ld (hl),WELL_SPACE
 		inc hl
 		dec c
 		jp nz,next_column_well
@@ -211,12 +237,12 @@ next_column_well
 ;draw walls
 
 	ld hl,playfield
-	ld b,well_height+1
-	ld de,well_width+1
+	ld b,WELL_HEIGHT+1
+	ld de,WELL_WIDTH+1
 next_row_wall
-	ld (hl), well_wall_char	
+	ld (hl), WELL_WAL_CHAR	
 	add hl,de
-	ld (hl),well_wall_char
+	ld (hl),WELL_WAL_CHAR
 	inc hl
 	dec b
 	jp nz,next_row_wall
@@ -224,9 +250,9 @@ next_row_wall
 ;do bottom row	
 	or a
 	sbc hl,de
-	ld b,well_width
+	ld b,WELL_WIDTH
 floor
-	ld (hl),well_wall_char
+	ld (hl),WELL_WAL_CHAR
 	inc hl
 	dec b
 	jp nz, floor	
@@ -243,6 +269,7 @@ start_screen
 ;****           init_game:setup game variables                ****
 ;*****************************************************************
 init_game
+	
 	;get randon 1st tetro
 	call get_tetro
 	;use this to work out next tetro (not quite randomn!)
@@ -262,7 +289,10 @@ init_game
 	;clear the score
 
 	;clear the level
-
+	ld a,LEVEL1_DELAY
+	ld (delay_counter), a
+	ld a, $01
+	ld (level),a
 
 	ret
 ;****************************************************************
@@ -281,9 +311,9 @@ get_keyboard
 	ld d,c
 	inc d
 	jr z, 1F
-	call DECODE
-	ld a,(hl)
-	ret
+		call DECODE
+		ld a,(hl)
+		ret
 1
 	ld a,$ff
 	ret
@@ -292,8 +322,25 @@ get_keyboard
 ;****        get_tetro: random tetro number                   ****
 ;*****************************************************************
 get_tetro
-	ld de,tetro_counter
-			ld a,(tetro_counter)
+	ld a,(tetro_counter)
+	ld (current_tetro),a
+	ret
+
+;****************************************************************
+;****        drop_tetro: move tetro_down                     ****
+;*****************************************************************
+drop_tetro
+	ld a,(Display+3)
+	inc a
+	ld (Display+3),a
+	
+	call undraw_tetro
+	
+	ld a,(tetro_y)
+	inc a
+	ld (tetro_y),a
+	
+	
 	ret
 
 ;****************************************************************
@@ -308,6 +355,85 @@ bump_tetro_counter
 carry_on
 	ld (tetro_counter),a
 	ret
+
+;****************************************************************
+;****    undraw_tetro:delete tetro from Playfield              ****
+;*****************************************************************
+undraw_tetro
+
+
+;Find out tetro start, using tetro number and rotation state
+
+;step 1 which tetro
+	ld hl,tetrominoZero
+	ld a,(current_tetro)
+	cp a,0
+	jp z,2F
+	ld de,TETRO_BLOCK*4
+1	
+		adc hl,de
+		dec a
+		jp nz,1B
+2
+
+;step 2 which rotation state
+	ld a,(tetro_rotation)
+	cp a,0
+	jp z,2F
+	ld de,TETRO_BLOCK
+1	
+		adc hl,de
+		dec a
+		jp nz,1B
+2
+	push hl
+
+;get start of playfield plot area
+	ld hl,playfield
+	ld a,(tetro_y)
+	cp a,0
+	jp z,2F
+	ld de,WELL_WIDTH+2	
+1	
+		adc hl,de
+		dec a
+		jp nz,1B
+2
+	ld a,(tetro_x)
+	ld e,a
+	ld d,0
+	add hl,de
+	inc hl
+
+;now draw the tetro into playfield
+	pop de
+	ld b,TETRO_SIZE
+1
+	push bc
+	ld b,TETRO_SIZE	
+2		
+		ld a,(de)
+		cp a, $40
+		jp z,3F	
+		cp a,$23
+		jp nz,3F
+			ld a,WELL_SPACE
+			ld (hl),a
+3		
+		inc hl
+		inc de
+		djnz 2B
+	push de
+	ld de, WELL_WIDTH+2-TETRO_SIZE
+	add hl,de
+	pop de
+	pop bc
+	djnz 1B
+	ret
+
+
+
+
 
 ; ===========================================================
 ; code ends
